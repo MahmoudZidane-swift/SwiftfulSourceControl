7368127e716945486f2b82de38579b6a7a6316e5
Mahmoud <mahmoud.m.zidane@gmail.com>
2024-10-21 22:52:20 -0700

    Initial Commit

---
+++ SwiftfulSourceControl/SwiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlApp.swift
+//  SwiftfulSourceControl
+//
+//  Created by Mahmoud.M,Zidane on 21/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftfulSourceControlTests/SwiftfulSourceControlTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlTests.swift
+//  SwiftfulSourceControlTests
+//
+//  Created by Mahmoud.M,Zidane on 21/10/2024.
+//
+
+import Testing
+@testable import SwiftfulSourceControl
+
+struct SwiftfulSourceControlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
