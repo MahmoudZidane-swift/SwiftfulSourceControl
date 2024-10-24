b8e93e94a865bc028f5c43d5459c105a2ea36270
Mahmoud <mahmoud.m.zidane@gmail.com>
2024-10-24 00:33:27 -0700

    My first commit

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "heart.fill")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .font(.largeTitle)
+                .foregroundStyle(.tint)
         }
         .padding()
     }
