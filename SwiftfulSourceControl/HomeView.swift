9e666baa631583f55f2df602efc391e56d6c7270
Mahmoud <mahmoud.m.zidane@gmail.com>
2024-10-24 00:50:21 -0700

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Mahmoud.M,Zidane on 24/10/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .font(.title2)
+    }
+}
+
+#Preview {
+    HomeView()
+}
