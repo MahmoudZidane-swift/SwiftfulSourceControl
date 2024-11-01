35aa4011983e4544b59687dffbf864f84ce017fc
Mahmoud <mahmoud.m.zidane@gmail.com>
2024-10-31 19:57:12 -0700

    [Feature] Add Hello word text with some change on text .

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -16,6 +16,9 @@ struct ContentView: View {
             Text("Hello, world!")
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
+            Text("Hello, world!")
+                .font(.largeTitle)
+                .foregroundStyle(.tint)
             Button("Click me"){
                 
             }
