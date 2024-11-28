d8cd900738dbd55051b30b8d82aa2a8b6d7d72d5
Mahmoud <mahmoud.m.zidane@gmail.com>
2024-11-27 21:52:24 -0800

    [Feature] Add ProfileView

---
+++ SwiftfulSourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Mahmoud.M,Zidane on 27/11/2024.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile View!")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
